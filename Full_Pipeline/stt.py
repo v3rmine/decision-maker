5f80c7a278f6e54676ac1dbd0bc860433f9e11b6
Johan Planchon <dev@johan.moe>
2024-01-25 10:35:07 +0100

    feat: added good stt

--- Full_Pipeline/stt.py
+++ Full_Pipeline/stt.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S poetry run python3
 from openai import OpenAI
+from pydub import AudioSegment
+from pydub.audio_segment import NamedTemporaryFile
+from pydub.silence import db_to_float
 import sounddevice as sd
 import soundfile as sf
 
@@ -7,17 +10,44 @@ import soundfile as sf
 client = OpenAI()
 
 # Set the recording parameters
-duration = 5
 samplerate = 48000  # Sample rate (Hz)
-temp = open("temp.wav", 'w+b')
 
-print(f"Record for {duration}s")
-rec = sd.rec(samplerate * duration, samplerate=samplerate, channels=1)
-sd.wait()
-sf.write(temp, rec, samplerate)
-print(f"Recording finished in {temp.name}.")
+# Record audio until silence is detected
+myrecording: list[AudioSegment] = []
+mean_sound_level = 0.5
 
-# Transcribe the audio
-transcription = client.audio.transcriptions.create(model="whisper-1", file=temp, response_format="verbose_json")
-# Print the transcription
-print(transcription.text)
+print("Recording...")
+for i in range(5):
+    rec = sd.rec(samplerate * 2, samplerate=samplerate, channels=1)
+    sd.wait()
+
+    with NamedTemporaryFile(suffix=".wav") as temp:
+        sf.write(temp, rec, samplerate, format="wav")
+
+        # Get the sound level of the audio segment
+        segment: AudioSegment = AudioSegment.from_wav(temp)
+        myrecording.append(segment)
+        sound_level = db_to_float(segment.dBFS)
+        print(mean_sound_level, sound_level)
+
+        # If the sound level is significantly lower than the previous sound level, break the loop
+        if sound_level < mean_sound_level - 0.05:
+            break
+        else:
+            mean_sound_level = (sound_level + mean_sound_level) / 2
+
+print("Recording finished.")
+
+# Save the recorded audio to a file
+audio = AudioSegment.empty()
+for segment in myrecording:
+    audio += segment
+
+with NamedTemporaryFile(suffix=".wav") as record:
+    audio.export(record, format="wav")
+    print(f"Audio saved")
+
+    # Transcribe the audio
+    transcription = client.audio.translations.create(model="whisper-1", file=record.file, response_format="verbose_json")
+    # Print the transcription
+    print(transcription)
